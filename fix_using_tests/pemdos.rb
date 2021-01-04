91611ebee81ef3e9e9d813bda10b9eb992812696
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 12:24:05 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
-# don't forget to add: require 'pry'
+# require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+#    10 * "s" + string
+    snake = ("s" * 10) + string
+ #   snake + string
+# binding.pry
   else
     string
   end
 end
+
